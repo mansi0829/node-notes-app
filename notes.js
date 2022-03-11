c5db03d68d3dfc2b8019a8ad8c6cb271d6cb0146
mansi0829 <mansigundre1@gmail.com>
2022-03-11 20:21:14 +0530

    module removes

--- notes.js
+++ notes.js
@@ -1,8 +1,51 @@
+const fs = require("fs");
 
 const getNotes = () => {
   return "Your Notes ....";
 };
 
+//adding a note
+const addNotes = (title, body) => {
+  const notes = loadNotes();
+
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.log("Note Title Taken");
+  }
+};
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+//removing a note
+const removeNotes = (title) => {
+};
+
 module.exports = {
   getNotes: getNotes,
+  addNotes: addNotes,
+  removeNotes: removeNotes,
 };
